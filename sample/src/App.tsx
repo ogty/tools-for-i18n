50bc04d2b64f9ac6e26b90a24de0a1710ad54e46
ogty <mofuiti1803@gmail.com>
2022-08-02 05:39:50 +0900

    delete: sample

--- sample/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import { useTranslation } from "react-i18next";
-
-function App() {
-  const { t } = useTranslation();
-  return <h1>{t("hello")}</h1>;
-}
-
-export default App;
