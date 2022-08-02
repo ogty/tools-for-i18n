fa288daacfc3cdd391ac9386684f8ec0755b55ad
ogty <mofuiti1803@gmail.com>
2022-08-03 00:21:43 +0900

    update

--- settings.py
+++ settings.py
@@ -24,12 +24,12 @@ Options:
                     Show this help message and exit
 """
 
-parser = argparse.ArgumentParser(description="Segment translation files by language")
-parser.add_argument("--file",          "-f", type=str, help="Path to the translation file")
-parser.add_argument("--output",        "-o", type=str, help="Path to the output directory")
-parser.add_argument("--file_name",     "-n", type=str, help="Output file name")
-parser.add_argument("--base_language", "-b", type=str, help="Most reliable language")
-parser.add_argument("--languages",     "-l", type=str, nargs="+", help="Languages to be segmented")
-parser.add_argument("--reverse",       "-r", action="store_true", help="Reverse translation")
-parser.add_argument("--table",         "-t", action="store_true", help="Output translation table")
-parser.add_argument("--empty",         "-e", action="store_true", help="Output empty translation")
+PARSER = argparse.ArgumentParser(description="Segment translation files by language")
+PARSER.add_argument("--file",          "-f", type=str, help="Path to the translation file")
+PARSER.add_argument("--output",        "-o", type=str, help="Path to the output directory")
+PARSER.add_argument("--file_name",     "-n", type=str, help="Output file name")
+PARSER.add_argument("--base_language", "-b", type=str, help="Most reliable language")
+PARSER.add_argument("--languages",     "-l", type=str, nargs="+", help="Languages to be segmented")
+PARSER.add_argument("--reverse",       "-r", action="store_true", help="Reverse translation")
+PARSER.add_argument("--table",         "-t", action="store_true", help="Output translation table")
+PARSER.add_argument("--empty",         "-e", action="store_true", help="Output empty translation")
