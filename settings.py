0565f10728bc31f509fecb8181ba9ad88b58dfe0
ogty <mofuiti1803@gmail.com>
2022-08-03 05:29:42 +0900

    update

--- settings.py
+++ settings.py
@@ -1,36 +1,96 @@
 import argparse
+from typing import TypedDict
 
-EXPORT_FILE_NAME = "translations.json"
-EMPTY_DATA_NAME = "EMPTY"
-CHARACTER_CODE = "utf-8"
-TABLE_TEMPLATE = "<table><tr><td>Path</td><td>---</td></tr><tr></tr>---</table>"
-HELP_MESSAGE = """
-Usage:
-    ./i18nseg [OPTIONS]
 
-Options:
-    -f, --file FILE
-                    Path to the translation file
-    -l, --languages LANGUAGE ...
-                    Languages to be segmented
-    -b, --base_language LANGUAGE
-                    Most reliable language
-    -o, --output DIRECTORY
-                    Path to the output directory
-    -t, --table
-                    Output translation table
-    -n, --file_name FILE
-                    Output file name
-    -h, --help
-                    Show this help message and exit
-"""
 INDENT = 4
 PARSER = argparse.ArgumentParser(description="Segment translation files by language")
-PARSER.add_argument("--file",          "-f", type=str, help="Path to the translation file")
-PARSER.add_argument("--output",        "-o", type=str, help="Path to the output directory")
-PARSER.add_argument("--file_name",     "-n", type=str, help="Output file name")
-PARSER.add_argument("--base_language", "-b", type=str, help="Most reliable language")
-PARSER.add_argument("--languages",     "-l", type=str, nargs="+", help="Languages to be segmented")
-PARSER.add_argument("--reverse",       "-r", action="store_true", help="Reverse translation")
-PARSER.add_argument("--table",         "-t", action="store_true", help="Output translation table")
-PARSER.add_argument("--empty",         "-e", action="store_true", help="Output empty translation")
+COMMAND_NAME = "i18nseg"
+HELP_MESSAGE = ''
+CHARACTER_CODE = "utf-8"
+TABLE_TEMPLATE = "<table><tr><td>Path</td><td>---</td></tr><tr></tr>---</table>"
+EMPTY_DATA_NAME = "EMPTY"
+EXPORT_FILE_NAME = "translations.json"
+
+argument_type = TypedDict("argument_type", {
+    "long": str,
+    "short": str,
+    "help": str,
+    "argument_name": str,
+    "type": str or None,
+    "action": str or None,
+    "nargs": str or None,
+})
+
+arguments = [
+    argument_type(
+        long="--file",
+        short="-f",
+        help="Path to the translation file",
+        type=str,
+        argument_name="<FILE>",
+    ),
+    argument_type(
+        long="--output",
+        short="-o",
+        help="Path to the output directory",
+        type=str,
+        argument_name="<DIRECTORY>",
+    ),
+    argument_type(
+        long="--file_name",
+        short="-n",
+        help="Output file name",
+        type=str,
+        argument_name="<FILE>",
+    ),
+    argument_type(
+        long="--base_language",
+        short="-b",
+        help="Most reliable language",
+        type=str,
+        argument_name="<LANGUAGE>",
+    ),
+    argument_type(
+        long="--languages",
+        short="-l",
+        help="Languages to be segmented",
+        type=str,
+        nargs="+",
+        argument_name="<LANGUAGE [...]>",
+    ),
+    argument_type(
+        long="--reverse",
+        short="-r",
+        help="Reverse translation",
+        action="store_true",
+        argument_name="",
+    ),
+    argument_type(
+        long="--table",
+        short="-t",
+        help="Output translation table",
+        action="store_true",
+        argument_name="",
+    ),
+    argument_type(
+        long="--empty",
+        short="-e",
+        help="Output empty translation",
+        action="store_true",
+        argument_name="",
+    )
+]
+
+HELP_MESSAGE += f"Usage:\n\t{COMMAND_NAME} [OPTIONS]\n\nOptions:\n"
+for argument in arguments:
+    long = argument["long"]
+    short = argument["short"]
+    HELP_MESSAGE += "\t%s, %s %s\n\t\t\t\t\t%s\n" % (
+        short, long, argument["argument_name"], argument["help"]
+    )
+
+    argument.pop("long")
+    argument.pop("short")
+    argument.pop("argument_name")
+
+    PARSER.add_argument(long, short, **argument)
